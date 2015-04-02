3560d613ba5a3f0d4a4bd0b17c28849a17eb61a9
ponceta <arnaud.poncet@swisstopo.ch>
2015-04-02 10:16:53 +0200

    Add WMS layer to print test

--- test/selenium/print_test.js
+++ test/selenium/print_test.js
@@ -19,10 +19,28 @@ var runTest = function(cap, driver, target) {
   driver.findElement(webdriver.By.xpath("//*[@id='import-kml-popup']/div[2]/div/div/div[1]/div/div[2]/form/input[1]")).sendKeys('http://opendata.utou.ch/urbanproto/geneva/geo/kml/Routes.kml');
   // Click on "KML Laden"
   driver.findElement(webdriver.By.xpath("//*[@id='import-kml-popup']/div[2]/div/div/div[2]/button")).click();
-  // Check if the KML has been parsed correctly
+  // Check if the KML was correctly parsed 
   driver.findElement(webdriver.By.xpath("//*[contains(text(), 'Parsing erfolgreich')]"));
   // Close popup
   driver.findElement(webdriver.By.xpath("//*[@id='import-kml-popup']/div[1]/div[2]/button[3]")).click();
+  // Click on "WMS Import"
+  driver.findElement(webdriver.By.xpath("//*[contains(text(), 'WMS Import')]")).click();
+  // Click on the URL input field
+  driver.findElement(webdriver.By.xpath("//*[@id='import-wms-popup']/div[2]/div/div/div/form/span/input[2]")).click();
+  // Write URL of the chosen WMS
+  driver.findElement(webdriver.By.xpath("//*[@id='import-wms-popup']/div[2]/div/div/div/form/span/input[2]")).sendKeys('http://wms.geo.admin.ch/');
+  // Click on "Verbinden"
+  driver.findElement(webdriver.By.xpath("//*[@id='import-wms-popup']/div[2]/div/div/div/form/button")).click();
+  // Click on "AGNES"
+  driver.findElement(webdriver.By.xpath("//*[@id='import-wms-popup']/div[2]/div/div/div/div[1]/div[2]/ul/li[4]/div/div")).click();
+  // Click on "Layer hinzufügen"
+  driver.findElement(webdriver.By.xpath("//*[@id='import-wms-popup']/div[2]/div/div/div/div[2]/button")).click();
+  // Accept the alert
+  driver.switchTo().alert().accept();
+  // Check if the WMS was correctly parsed
+  driver.findElement(webdriver.By.xpath("//*[contains(text(), 'WMS Layer erfolgreich geladen')]"));
+  // Close popup
+  driver.findElement(webdriver.By.xpath("//*[@id='import-wms-popup']/div[1]/div[2]/button[3]")).click();
   // Click on "Drucken"
   driver.findElement(webdriver.By.xpath("//a[@id='printHeading']")).click();
   // Wait until print is opened and animation is finished
